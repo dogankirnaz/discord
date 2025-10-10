868b505910aebe10bcd5cd2e15c1e4fa2889c2f1
dogankirnaz <kirnazdogan@gmail.com>
2025-10-10 13:49:00 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from discord import app_commands
 TOKEN = os.getenv("DISCORD_TOKEN")
 SERVER = int(os.getenv("SERVER_ID"))
 
+#BOT
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+#GLOBAL
+
+# EVENTS
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
     try:
         guild = discord.Object(id=SERVER)
 
-        # Sync main server
+        # Sync test server
         guild_synced = await bot.tree.sync(guild=guild)
         print(f"Synced {len(guild_synced)} command(s) to the guild.")
 
@@ -27,8 +31,12 @@ async def on_ready():
     except Exception as e:
         print(e)
         
+# COMMANDS
 @bot.tree.command(name="hello", description="Say hello!")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hi, {interaction.user.mention}!")
+    await interaction.response.send_message(
+        f"Hi, {interaction.user.mention}!", 
+        ephemeral=True
+    )
 
 bot.run(TOKEN)
